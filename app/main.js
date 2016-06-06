ae3a5f8470488ded38d2af3c1575f398750b4c38
FaQ <fcostazini@gmail.com>
2016-06-06 19:59:16 -0300

    Elimino maps y JS para evitar conflictos en los merge

--- app/main.js
+++
@@ -1,9 +0,0 @@
-var application_1 = require("nativescript-angular/application");
-// >> using-global-directives
-var side_drawer_directives_1 = require("nativescript-telerik-ui-pro/sidedrawer/angular/side-drawer-directives");
-var listview_directives_1 = require('nativescript-telerik-ui-pro/listview/angular/listview-directives');
-var main_component_1 = require("./main/main.component");
-// << (hide)
-application_1.nativeScriptBootstrap(main_component_1.MainComponent, [listview_directives_1.LISTVIEW_PROVIDERS, side_drawer_directives_1.SIDEDRAWER_PROVIDERS]);
-// << using-global-directives 
-//# sourceMappingURL=main.js.map
